8b74ae762482971d9e52bbf3823825b772ee9627
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-10-18 11:21:01 +0100

    Add Pydantic data objects and translate endpoint
    This is the outline endpoint. For now it's returning a fixed phrase, and we test whether it returns what's expected. Next I'll write the actual translation service

--- tech_test/app/routes.py
+++ tech_test/app/routes.py
@@ -1,7 +1,24 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
 
 api_router = APIRouter()
+
 
 @api_router.get("/")
 def index():
     return {"message": "Hello world"}
+
+
+class TranslationInput(BaseModel):
+    input_language: str = "en"
+    output_language: str
+    input_text: str
+
+
+class TranslationOutput(TranslationInput):
+    output_text: str = "Coming soon!"
+
+
+@api_router.post("/translate", response_model=TranslationOutput)
+def translate(data: TranslationInput) -> dict:
+    return data.model_dump()
--- tests/test_main.py
+++ tests/test_main.py
@@ -13,3 +13,14 @@ def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
+
+
+def test_translate(client):
+    post_data = {
+        "input_language": "en",
+        "output_language": "fr",
+        "input_text": "Hello world!",
+    }
+    response = client.post("/translate", json=post_data)
+    response_dict = response.json()
+    assert response_dict["output_text"] == "Coming soon!"
