572ba570dc6e8442f2ce3640e15c3615fff143f0
jonathan.kerr <3410350+jonodrew@users.noreply.github.com>
2025-10-18 11:15:17 +0100

    Add fixture for test client
    This removes the need to duplicate code by instantiating the client

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,10 +1,15 @@
+import pytest
 from fastapi.testclient import TestClient
+
 from tech_test.app.main import app_factory
 
-def test_index():
-    app = app_factory()
-    client = TestClient(app)
 
+@pytest.fixture
+def client() -> TestClient:
+    return TestClient(app_factory())
+
+
+def test_index(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": "Hello world"}
